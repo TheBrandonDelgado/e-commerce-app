8a786dc0b9fae0343b80dc0503df5e0e94ac8b00
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-08-13 16:53:23 -0500

    Add ability to connect to db

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    pool.query('SELECT * FROM users', (err, res) => {
+        if (err) {
+          console.error('Error executing query', err.stack);
+        } else {
+          console.log('Query result:', res.rows);
+        }
+        pool.end();
+    });
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
