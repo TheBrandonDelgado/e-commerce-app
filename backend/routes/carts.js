9184fa6ec60ddb64eaf47b374bf8c2e9ac026711
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-09-22 15:58:06 -0500

    Set up checkout endpoint

--- backend/routes/carts.js
+++ backend/routes/carts.js
@@ -38,4 +38,40 @@ router.delete('/:cartId', (req, res) => {
     pool.end();
 });
 
+router.post('/:cartId/items', (req, res) => {
+    const { cartId } = req.params;
+    const { productId, quantity } = req.body;
+    pool.query('INSERT INTO cart_items (cart_id, product_id, quantity) VALUES ($1, $2, $3) RETURNING *', [cartId, productId, quantity], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows[0]);
+        }
+    });
+    pool.end();
+});
+
+router.delete('/:cartId/items/:itemId', (req, res) => {
+    const { cartId, itemId } = req.params;
+    pool.query('DELETE FROM cart_items WHERE id = $1 AND cart_id = $2', [itemId, cartId], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows);
+        }
+    });
+    pool.end();
+}); 
+
+router.post('/:cartId/checkout', (req, res) => {
+    const { cartId } = req.params;
+    pool.query('SELECT * FROM cart_items WHERE cart_id = $1', [cartId], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
 module.exports = router;
