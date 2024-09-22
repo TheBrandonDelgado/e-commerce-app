c21bfcf34d2265c67c4cf08c58d4c20f3f835890
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-09-22 16:00:50 -0500

    Set up order endpoints

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,20 +1,65 @@
 const express = require('express');
 const router = express.Router();
+const { pool } = require('../lib/pool');
 
 router.get('/', (req, res) => {
-    res.send('GET Orders');
+    pool.query('SELECT * FROM orders', (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows);
+        }
+    });
+    pool.end();
 });
 
 router.post('/', (req, res) => {
-    res.send('POST Orders');
+    const { userId, cartId, status, total_amount, shipping_address, payment_method } = req.body;
+    pool.query('INSERT INTO orders (user_id, cart_id, status, total_amount, shipping_address, payment_method) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [userId, cartId, status, total_amount, shipping_address, payment_method], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows[0]);
+        }
+    });
+    pool.end();
 });
 
-router.put('/', (req, res) => {
-    res.send('PUT Orders');
+router.get('/:orderId', (req, res) => {
+    const { orderId } = req.params;
+    pool.query('SELECT * FROM orders WHERE id = $1', [orderId], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows[0]);
+        }
+    });
+    pool.end();
 });
 
-router.delete('/', (req, res) => {
-    res.send('DELETE Orders');
+router.put('/:orderId', (req, res) => {
+    const { orderId } = req.params;
+    const { userId, cartId, status, total_amount, shipping_address, payment_method } = req.body;
+    pool.query('UPDATE orders SET user_id = $1, cart_id = $2, status = $3, total_amount = $4, shipping_address = $5, payment_method = $6 WHERE id = $7', [userId, cartId, status, total_amount, shipping_address, payment_method, orderId], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows[0]);
+        }
+    });
+    pool.end();
+});
+
+router.delete('/:orderId', (req, res) => {
+    const { orderId } = req.params;
+    pool.query('DELETE FROM orders WHERE id = $1', [orderId], (err, result) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(result.rows);
+        }
+    });
+    pool.end();
 });
 
 module.exports = router;
